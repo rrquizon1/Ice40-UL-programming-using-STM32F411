9f96b9777cbd26fdac0b420ef8d2e3f20d5ac5c5
rrquizon1 <70574862+rrquizon1@users.noreply.github.com>
2024-11-03 14:35:04 +0800

    Add files via upload

---
+++ Core/Inc/data.h
@@ -0,0 +1,16 @@
+/*
+ * data.h
+ *
+ *  Created on: Sep 8, 2024
+ *      Author: Rhodz
+ */
+
+#ifndef INC_DATA_H_
+#define INC_DATA_H_
+
+extern const int g_iDataSize;  // Declaration of the integer
+extern unsigned char g_pucDataArray[];  // Declaration of the array
+
+
+
+#endif /* INC_DATA_H_ */
